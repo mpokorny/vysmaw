e0929740b9adf4461efb43c5960065be2d5790e5
Martin Pokorny <mpokorny@nrao.edu>
2016-11-29 15:05:25 -0700

    Add copyright statements

--- src/vysmaw.c
+++ src/vysmaw.c
@@ -1,3 +1,20 @@
+//
+// Copyright © 2016 Associated Universities, Inc. Washington DC, USA.
+//
+// This file is part of vysmaw.
+//
+// vysmaw is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// vysmaw is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// vysmaw.  If not, see <http://www.gnu.org/licenses/>.
+//
 #include <vysmaw_private.h>
 #include <glib.h>
 #include <string.h>
--- src/vysmaw.h
+++ src/vysmaw.h
@@ -1,8 +1,20 @@
-/*
- * maw /mô/
- *   noun
- *     the jaws or throat of a voracious animal
- */
+//
+// Copyright © 2016 Associated Universities, Inc. Washington DC, USA.
+//
+// This file is part of vysmaw.
+//
+// vysmaw is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// vysmaw is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// vysmaw.  If not, see <http://www.gnu.org/licenses/>.
+//
 #ifndef VYSMAW_H_
 #define VYSMAW_H_
 
