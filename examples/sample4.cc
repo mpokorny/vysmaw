f065be934b518662171d5c725c96d2e27babf02b
Martin Pokorny <mpokorny@nrao.edu>
2017-06-01 10:42:52 -0600

    Update message types in sample4

--- examples/sample4.cc
+++ examples/sample4.cc
@@ -83,7 +83,7 @@ show_counters(array<unsigned,VYSMAW_MESSAGE_END + 1> &counters)
 	const unordered_map<enum vysmaw_message_type,string> names = {
 		{VYSMAW_MESSAGE_VALID_BUFFER, "valid-buffer"},
 		{VYSMAW_MESSAGE_ID_FAILURE, "id-failure"},
-		{VYSMAW_MESSAGE_QUEUE_OVERFLOW, "queue-overflow"},
+		{VYSMAW_MESSAGE_QUEUE_ALERT, "queue-alert"},
 		{VYSMAW_MESSAGE_DATA_BUFFER_STARVATION, "data-buffer-starvation"},
 		{VYSMAW_MESSAGE_SIGNAL_BUFFER_STARVATION, "signal-buffer-starvation"},
 		{VYSMAW_MESSAGE_SIGNAL_RECEIVE_FAILURE, "signal-receive-failure"},
@@ -100,7 +100,7 @@ show_counters(array<unsigned,VYSMAW_MESSAGE_END + 1> &counters)
 	const enum vysmaw_message_type msg_types[] = {
 		VYSMAW_MESSAGE_VALID_BUFFER,
 		VYSMAW_MESSAGE_ID_FAILURE,
-		VYSMAW_MESSAGE_QUEUE_OVERFLOW,
+		VYSMAW_MESSAGE_QUEUE_ALERT,
 		VYSMAW_MESSAGE_DATA_BUFFER_STARVATION,
 		VYSMAW_MESSAGE_SIGNAL_BUFFER_STARVATION,
 		VYSMAW_MESSAGE_SIGNAL_RECEIVE_QUEUE_UNDERFLOW,
